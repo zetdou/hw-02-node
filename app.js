8b9097a4f58f632b2f595d660484bd634b0c20d5
zetdou <zyziupoz@gmail.com>
2024-09-02 21:39:27 +0200

    auth.js
    Add authRouter to app. Create signup and login method working good. Add function setPassword and validatePassword to userSchema

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({
@@ -30,5 +32,6 @@ app.use((err, req, res, next) => {
     message: err.message,
   });
 });
+
 
 module.exports = app;
